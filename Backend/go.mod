9a1bb6f7780fead0156856413a82796a697eb498
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2022-10-22 16:00:21 -0400

    backend api setup

--- Backend/go.mod
+++ Backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/AdamEbrahim/HackDuke22/Backend
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0
--- Backend/main.go
+++ Backend/main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"github.com/joho/godotenv"
+	"errors"
+	//"database/sql"
+	//"gorm.io/driver/postgres"
+  	//"gorm.io/gorm"
 )
 
+func init() {
+	if _, err := os.Stat("./.env"); errors.Is(err, os.ErrNotExist) {
+		// .env file does not exist (production)
+		fmt.Println("cant find env")
+	} else {
+		// .env file exists (development)
+		err2 := godotenv.Load(".env")
+
+		if err2 != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	
+
+}
+
+func testing(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("test Working")
+
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("App Running")
+	port := os.Getenv("PORT")
+	fmt.Println(port)
+
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/test", testing)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
